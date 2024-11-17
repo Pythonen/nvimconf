e5270ddb399f9280fa54e0c83461ec1aee4a28f2
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2024-11-17 21:03:30 +0200

    Update breaking changes

--- lua/conf/remap.lua
+++ lua/conf/remap.lua
@@ -19,11 +19,8 @@ local harpoon = require('harpoon')
 
 harpoon:setup()
 
-
-local ui = require('harpoon.ui')
-
-vim.keymap.set('n', "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set('n', "<C-w>", function() ui.toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set('n', "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set('n', "<C-w>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 vim.keymap.set('n', "<S-q>", function() harpoon:list():select(1) end)
 vim.keymap.set('n', "<S-w>", function() harpoon:list():select(2) end)
 vim.keymap.set('n', "<S-e>", function() harpoon:list():select(3) end)
