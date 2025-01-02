f4dccfed2995b4c80a6ac9847f3ab552cffa96d2
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2025-01-02 19:51:10 +0200

    show the whole fiel path in statusbar

--- init.lua
+++ init.lua
@@ -160,6 +160,7 @@ else
       theme = 'gruvbox',
       component_separators = '|',
       section_separators = '',
+      path = 1
     },
   }
 
@@ -303,6 +304,7 @@ else
     nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
     nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
 
     -- See `:help K` for why this keymap
     nmap('gh', vim.lsp.buf.hover, 'Hover Documentation')
